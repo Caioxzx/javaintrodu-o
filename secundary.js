69af193bedbf01c166afbe0137a43b7d6b02c9a9
Caioxzx <134522924+Caioxzx@users.noreply.github.com>
2023-10-27 12:09:56 +0000

    testes site

---
+++ secundary.js
@@ -0,0 +1,62 @@
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play();
+}
+
+playSound('pom.mp3');
+
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play();
+}
+
+playSound('clap.mp3');
+
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play();
+}
+
+playSound('tim.mp3');
+
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play();
+}
+
+playSound('puff.mp3');
+
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play('');
+}
+
+playSound('splash.mp3');
+
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play();
+}
+
+playSound('toim.mp3');
+
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play();
+}
+
+playSound('psh.mp3');
+
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play();
+}
+
+playSound('tic.mp3');
+
+function playSound(soundFile) {
+  var audio = new Audio(soundFile);
+  audio.play();
+}
+
+playSound('tom.mp3');
